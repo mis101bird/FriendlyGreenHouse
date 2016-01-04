68595b9bc0aec3dc10166f3501cd03e3dc8ba253
Saf <winterersaf@gmail.com>
2016-01-04 19:00:42 +0800

    456

--- app/src/main/java/com/example/friendlygreenhouse/application/DiaryActivity.java
+++ app/src/main/java/com/example/friendlygreenhouse/application/DiaryActivity.java
@@ -2,6 +2,7 @@ package com.example.friendlygreenhouse.application;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -18,6 +19,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 import android.os.Build;
 import com.example.friendlygreenhouse.application.LocalDatabase.SQLiteHandler;
@@ -42,6 +45,12 @@ public class DiaryActivity extends Activity{
     private Button mBtnAdd,
             mBtnQuery,
             mBtnList;
+
+    private TextView tvDate;
+    private TextView tvTime;
+    private TextView btDate;
+    private TextView btTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +89,65 @@ public class DiaryActivity extends Activity{
         mBtnAdd.setOnClickListener(btnAddOnClick);
         mBtnQuery.setOnClickListener(btnQueryOnClick);
         mBtnList.setOnClickListener(btnListOnClick);
+
+        tvDate = (TextView) findViewById(R.id.edtSex);
+        //tvTime = (TextView) findViewById(R.id.tvTime);
+
+        //btDate = (Button) findViewById(R.id.edtSex);
+        //btTime = (Button) findViewById(R.id.btTime);
+
+        tvDate.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+        /*
+        btTime.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                showTimePickerDialog();
+            }
+
+        });
+        */
+    }
+
+    public void showDatePickerDialog() {
+        // 設定初始日期
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // 跳出日期選擇器
+        DatePickerDialog dpd = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        // 完成選擇，顯示日期
+                        tvDate.setText(year + "-" + (monthOfYear + 1) + "-"
+                                + dayOfMonth);
+
+                    }
+                }, mYear, mMonth, mDay);
+        dpd.show();
+    }
+
+    public void showTimePickerDialog() {
+        // 設定初始時間
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        // 跳出時間選擇器
+        TimePickerDialog tpd = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+                        // 完成選擇，顯示時間
+                        tvTime.setText(hourOfDay + ":" + minute);
+                    }
+                }, mHour, mMinute, false);
+        tpd.show();
     }
 /*
     @Override
