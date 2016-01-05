81b6cef1836933e1d46215507b9e34fa06174af0
Saf <winterersaf@gmail.com>
2016-01-05 08:22:54 +0800

    456

--- app/src/main/java/com/example/friendlygreenhouse/application/DiaryActivity.java
+++ app/src/main/java/com/example/friendlygreenhouse/application/DiaryActivity.java
@@ -42,7 +42,7 @@ public class DiaryActivity extends Activity{
             mBtnQuery,
             mBtnList;
     private  final List<String> dd=new ArrayList<String>();
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
